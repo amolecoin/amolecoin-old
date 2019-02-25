7cebbabc389138e2d16e2b50ac199cfc5788d7cc
MENDEX <39565143+M3ND3X@users.noreply.github.com>
2019-02-25 14:29:35 +0300

    AmoleCoin

--- src/validation.cpp
+++ src/validation.cpp
@@ -1027,7 +1027,7 @@ CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
     if (halvings >= 64)
         return 0;
 
-    if (nHeight == 1) return COIN * 4000000;
+    if (nHeight == 1) return COIN * 5000000;
     CAmount nSubsidy = 50 * COIN;
     // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
     nSubsidy >>= halvings;
